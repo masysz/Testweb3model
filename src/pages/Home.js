c9296bb9b62e86cfb163a1b4b067a92b0225ba30
masysz <67843831+masysz@users.noreply.github.com>
2024-08-06 21:23:55 +0700

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import "../App.css";
+import { AnimatePresence } from "framer-motion";
+import Footer from "../Components/Footer";
